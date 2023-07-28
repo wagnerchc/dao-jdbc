4ae27fa2afba50507c4a1d58ced5005b2339167f
wagnerchc <w.chequeleiro@gmail.com>
2023-07-28 14:28:34 -0300

    Department entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,15 @@
 package application;
 
+import model.entities.Department;
+
 public class Program {
 
+	public static void main(String[] args) {
+		
+		Department obj = new Department(1, "Books");
+		
+		System.out.print(obj);
+		
+	}
+	
 }
